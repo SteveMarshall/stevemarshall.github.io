c1878335594b55f323a9f750bb4a82ad57e54b22
Steve Marshall <steve@nascentguruism.com>
2014-04-06 14:49:55 +0100

    Pass javascript class querystrings to other pages
    Ensure we don't drop the grid or whatever as one navigates the site.

--- assets/class-switcher.js
+++ assets/class-switcher.js
@@ -2,5 +2,12 @@ document.addEventListener( "DOMContentLoaded", function() {
   var classes = window.location.search.substr(1);
   classes = classes.replace(/-/g, ' ');
   document.body.className = classes;
+
+  var anchors = document.getElementsByTagName('a');
+  for (var i = 0; i < anchors.length; i++) {
+    if (0 === anchors[i].href.indexOf('http://' + window.location.host + '/')) {
+      anchors[i].href += window.location.search;
+    }
+  }
   document.removeEventListener( "DOMContentLoaded", arguments.callee, false );
 }, false );
