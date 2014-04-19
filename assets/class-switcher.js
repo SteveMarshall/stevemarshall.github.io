c2e41e21fa08df361c28040812f157360360418a
Steve Marshall <steve@nascentguruism.com>
2014-04-19 13:45:50 +0100

    Only add body class when there's a class to add

--- assets/class-switcher.js
+++ assets/class-switcher.js
@@ -1,13 +1,15 @@
 document.addEventListener( "DOMContentLoaded", function() {
   var classes = window.location.search.substr(1);
-  classes = classes.replace(/-/g, ' ');
-  document.body.className = classes;
+  if (0 < classes.length) {
+      classes = classes.replace(/-/g, ' ');
+      document.body.className = classes;
 
-  var anchors = document.getElementsByTagName('a');
-  for (var i = 0; i < anchors.length; i++) {
-    if (0 === anchors[i].href.indexOf('http://' + window.location.host + '/')) {
-      anchors[i].href += window.location.search;
-    }
+      var anchors = document.getElementsByTagName('a');
+      for (var i = 0; i < anchors.length; i++) {
+        if (0 === anchors[i].href.indexOf('http://' + window.location.host + '/')) {
+          anchors[i].href += window.location.search;
+        }
+      }
   }
   document.removeEventListener( "DOMContentLoaded", arguments.callee, false );
 }, false );
